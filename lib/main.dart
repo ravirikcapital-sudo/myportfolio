bd261bb8d07e06cb78b93366959cec2a513ab77d
ravirikcapital-sudo <ravi.rikcapital@gmail.com>
2026-01-19 15:29:27 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,11 @@ import 'screens/contact_screen.dart';
 import 'auth/login_screen.dart';
 import 'auth/signup_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
