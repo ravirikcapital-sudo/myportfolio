865d697810a482860c49addf6923b0a2980ab476
ravirikcapital-sudo <ravi.rikcapital@gmail.com>
2026-01-19 15:16:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,17 @@ import 'screens/contact_screen.dart';
 import 'auth/login_screen.dart';
 import 'auth/signup_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
